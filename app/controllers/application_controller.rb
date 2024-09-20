89ed55d2951e890b8bf215771a8ccf4260a221cd
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2024-09-20 22:30:21 +0100

    add some dynamic html features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
+# In app/controllers/application_controller.rb
+
 class ApplicationController < ActionController::Base
+  # Increment the number of HTTP requests that have been made
+  before_action :increment_HTTP_req_counter
+
+  # Generate a random number
+  before_action :set_random_number
+
+  private
+
+  def increment_HTTP_req_counter
+    HttpReqCounter.first_or_create.increment!(:count)
+  end
+
+  def set_random_number
+    # Generate a random number from 0 to 999,999
+    raw_number = rand(1_000_000)
+    
+    # Format the number with commas
+    @random_number = raw_number.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+  end
+
 end
--- prod-test.sh
+++ prod-test.sh
@@ -1,3 +1,3 @@
-bin/rails generate dockerfile --force --fullstaq --jemalloc --yjit --cache --parallel
+bin/rails generate dockerfile --force --fullstaq --jemalloc --yjit --cache --parallel --compose
 docker build -t personal-website-rails-webpack .
 docker compose up 
