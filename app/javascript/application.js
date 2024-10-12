b86c641244cdacd0a5414ce4eed001f6bb972eeb
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2024-10-12 16:21:13 +0100

    add glitchtip on the frontend as well

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import * as Sentry from '@sentry/browser';
+Sentry.init({
+    dsn: "https://464a84695ee24afbb22817b94618d577@glitchtip-cs40w800ggw0gs0k804skcc0.geor.me/5",
+    tracesSampleRate: 1.0,
+  });
