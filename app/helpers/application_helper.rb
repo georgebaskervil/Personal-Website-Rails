25073ed7092a3bfcbe439590b7a9258b34798822
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2025-03-16 00:26:09 +0000

    improve link titling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 module ApplicationHelper
   include BetterHtml::Helpers
+
+  def page_title(page_specific_title = nil)
+    base_title = "George Baskerville's Personal Website"
+    route_title = if page_specific_title.present?
+      page_specific_title
+    elsif request.path == "/"
+      nil
+    else
+      request.path.split("/").reject(&:empty?).map(&:titleize).join(" - ")
+    end
+    
+    route_title.present? ? "#{base_title} - #{route_title}" : base_title
+  end
 end
